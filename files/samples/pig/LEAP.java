a11f57c6e97e8bdad6afa7b6edad07d88670fb25
Enrique Martín <emartinm@fdi.ucm.es>
2016-11-17 14:52:23 +0100

    Contenido inicial

---
+++ files/samples/pig/LEAP.java
@@ -0,0 +1,18 @@
+package udf;
+import java.io.IOException;
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+
+public class LEAP extends EvalFunc<Boolean>
+{
+    public Boolean exec(Tuple input) throws IOException {
+        if (input == null || input.size() == 0 || input.get(0) == null)
+            return null;
+        try{
+            int number = ((Integer)input.get(0)).intValue();
+            return new Boolean(number%4==0 && number%100!=0);
+        }catch(Exception e){
+            throw new IOException("Caught exception processing input row ", e);
+        }
+    }
+}
---
+++ provision.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y default-jdk python-minimal
+
+wget http://apache.rediris.es/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
+tar xvzf hadoop-2.7.3.tar.gz
+
+wget http://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz
+tar xvzf spark-2.0.2-bin-hadoop2.7.tgz
+
+wget http://apache.cs.utah.edu/pig/latest/pig-0.16.0.tar.gz
+tar xvzf pig-0.16.0.tar.gz
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> .bashrc
+export CLASSPATH=` ./hadoop-2.7.3/bin/hadoop classpath`
+echo "export CLASSPATH=$CLASSPATH:` ./hadoop-2.7.3/bin/hadoop classpath`" >> .bashrc
+export PATH=$PATH:/home/ubuntu/hadoop-2.7.3/bin:/home/ubuntu/spark-2.0.2-bin-hadoop2.7/bin:/home/ubuntu/pig-0.16.0/bin
+echo "export PATH=$PATH:/home/ubuntu/hadoop-2.7.3/bin:/home/ubuntu/spark-2.0.2-bin-hadoop2.7/bin:/home/ubuntu/pig-0.16.0/bin" >> .bashrc
