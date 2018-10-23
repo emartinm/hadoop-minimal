078850a602fd036944b2e15bc5d658038a7ef7cd
Enrique Martín <emartinm@fdi.ucm.es>
2018-10-23 12:33:19 +0200

    Actualización de provision.sh

--- provision.sh
+++ provision.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y default-jdk python-minimal
+sudo apt install -y openjdk-8-jdk
 
-wget http://apache.rediris.es/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
-tar xvzf hadoop-2.7.3.tar.gz
+# Instala Anaconda en /home/vagrant/anaconda3
+wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
+chmod u+x Anaconda3-5.3.0-Linux-x86_64.sh
+~/Anaconda3-5.3.0-Linux-x86_64.sh -b
 
-wget http://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz
-tar xvzf spark-2.0.2-bin-hadoop2.7.tgz
+# Instala Hadoop en /home/vagrant/hadoop-3.1.1
+wget http://apache.uvigo.es/hadoop/common/hadoop-3.1.1/hadoop-3.1.1.tar.gz
+tar xvzf hadoop-3.1.1.tar.gz
 
-wget http://apache.cs.utah.edu/pig/latest/pig-0.16.0.tar.gz
-tar xvzf pig-0.16.0.tar.gz
+# Instala Spark en /home/vagrant/spark-2.3.2-bin-hadoop2.7
+wget http://apache.uvigo.es/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz
+tar xvzf spark-2.3.2-bin-hadoop2.7.tgz
 
+# Establece las variables de entorno y las almacena en el fichero ~/.bashrc
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> .bashrc
-export CLASSPATH=` ./hadoop-2.7.3/bin/hadoop classpath`
-echo "export CLASSPATH=$CLASSPATH:` ./hadoop-2.7.3/bin/hadoop classpath`" >> .bashrc
-export PATH=$PATH:/home/ubuntu/hadoop-2.7.3/bin:/home/ubuntu/spark-2.0.2-bin-hadoop2.7/bin:/home/ubuntu/pig-0.16.0/bin
-echo "export PATH=$PATH:/home/ubuntu/hadoop-2.7.3/bin:/home/ubuntu/spark-2.0.2-bin-hadoop2.7/bin:/home/ubuntu/pig-0.16.0/bin" >> .bashrc
+export CLASSPATH=`./hadoop-3.1.1/bin/hadoop classpath`
+echo "export CLASSPATH=$CLASSPATH:` ./hadoop-3.1.1/bin/hadoop classpath`" >> .bashrc
+export PATH=$PATH:/home/vagrant/hadoop-3.1.1/bin:/home/vagrant/spark-2.3.2-bin-hadoop2.7/bin:/home/vagrant/anaconda3/bin
+echo "export PATH=$PATH:/home/vagrant/hadoop-3.1.1/bin:/home/vagrant/spark-2.3.2-bin-hadoop2.7/bin:/home/vagrant/anaconda3/bin" >> .bashrc
+export PYSPARK_PYTHON=/home/vagrant/anaconda3/bin/python
+echo "export PYSPARK_PYTHON=/home/vagrant/anaconda3/bin/python" >> .bashrc
